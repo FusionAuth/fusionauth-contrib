3e8cc202f6269742dfda2eda773a116f0160282b
Dan Moore <dan@fusionauth.io>
2022-05-17 16:41:32 -0600

    Add rounds back in

--- Password Hashing Plugins/src/main/java/com/mycompany/fusionauth/plugins/ExampleFirebaseScryptPasswordEncryptor.java
+++ Password Hashing Plugins/src/main/java/com/mycompany/fusionauth/plugins/ExampleFirebaseScryptPasswordEncryptor.java
@@ -42,13 +42,14 @@ public class ExampleFirebaseScryptPasswordEncryptor implements PasswordEncryptor
     on the 3 dots near the the **Add User** button. Then select **Password hash parameters**.
     This will open a modal with the parameters needed below. Copy them here */
   private static final int memcost = 14;
+  private static final int rounds = 8;
   private static final String saltSep = "Bw==";
   private static final String base64_signer_key = "GhlZmw+EylyJl3fhsIOa+bmAEP1sYSHfZargl1unu/5mCp7pi818imxZOdSjM1558T+XU3cBflyr5HDghZqv7Q==";
 // end::scryptParameters[]
 
   @Override
   public int defaultFactor() {
-    return 8;
+    return rounds;
   }
 
   @Override
